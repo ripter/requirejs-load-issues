5de5dfe3288bc35db90af60f6cffdf22c40b938f
Benjamin Goering <bengoering@gmail.com>
2014-04-23 14:06:07 -0700

    works?

--- moduleB/build.config.js
+++ moduleB/build.config.js
@@ -1,16 +1,19 @@
 ({
-  name: 'src/js/main'
+  name: 'moduleB'
   , out: 'dist/bundle.js'
-
+  , optimize: 'none'
+  , include: ['almond']
   // wrap is required to load in require([])
-  , wrap: {
-    start: 'define(function(require, exports, module) {'
-    , end: '});'
-  }
-
   , paths: {
+    almond: '../lib/almond/almond',
+    'moduleB': 'src/js/main',
     // doesn't work either way
-    'jquery': 'empty:'
-    //'jquery': 'bower_components/jquery/dist/jquery'
+    // 'jquery': 'empty:'
+    'jquery': '../bower_components/jquery/dist/jquery'
+  },
+  cjsTranslate: true,
+  wrap: {
+    startFile: 'wrap-start.frag',
+    endFile: 'wrap-end.frag'
   }
 })
