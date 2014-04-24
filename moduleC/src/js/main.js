99af549fd2f4e4a0b9689d1eb2ea96442c205bcd
Chris Richards <ripter001@gmail.com>
2014-04-23 17:19:00 -0700

    direct to the cdn

--- moduleC/src/js/main.js
+++ moduleC/src/js/main.js
@@ -5,7 +5,7 @@
 //var moduleA = require('moduleA');
 //var moduleA = require('moduleA/dist/bundle');
 //var moduleA = require('http://localhost:8080/moduleA/dist/bundle.js');
-var moduleA = require('http://cdn.livefyre.com/libs/collection-manager/rc/main.js');
+var moduleA = require('http://livefyre-cdn.s3.amazonaws.com/libs/collection-manager/rc/main.js');
 
 module.exports = function() {
   console.log('moduleC exported function');
